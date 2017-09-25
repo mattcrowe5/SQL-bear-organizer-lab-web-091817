7c3d55aeb5768e5cfe22afb705a841d912daf7e9
mattcrowe5 <crowe.matthew5@gmail.com>
2017-09-24 23:46:36 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES ("Mr. Chocolate", 23, "M", 'brown', 'cool', 1);
+INSERT INTO bears VALUES ("Rowdy", 2, "F", 'black', 'cool', 1);
+INSERT INTO bears VALUES ("Tabitha", 3, "M", 'brown', 'mad', 0);
+INSERT INTO bears VALUES ("Sargeant Brown", 13, "M", 'brown', 'cool', 1);
+INSERT INTO bears VALUES ("Melissa", 7, "F", 'white', 'calm', 0);
+INSERT INTO bears VALUES ("Grinch", 63, "M", 'gold', 'cool', 0);
+INSERT INTO bears VALUES ("Wendy", 5, "F", 'purple', 'happy', 1);
+INSERT INTO bears VALUES (null, 23, "M", 'brown', 'cool', 1);
